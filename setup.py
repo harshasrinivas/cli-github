6b574c496f63706b92b0df8ddebdba2294c074a6
95harsha95 <95harsha95@gmail.com>
2015-06-27 04:15:33 +0530

    version change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(name = 'clipy-github',
       description = 'Github inside the Command Line',
-      version = '0.1.0',
+      version = '0.1.2',
       author = 'Harsha Srinivas',
       author_email = '95harsha95@gmail.com',
       packages = ['clipy_github'],
